f77c47a59baf27d7f2faa8949550f730970948bf
bhans-uvic <bhans@uvic.ca>
2023-09-24 14:53:43 -0700

    Changed Setip.bat for main

--- setup.bat
+++ setup.bat
@@ -1,11 +1,11 @@
-ECHO Creating Virtual Environment Spectral in current directory
+ECHO Creating Virtual Environment Spectral in current directory...
 pip install virtualenv
-virtualenv Spectral_Test
-cd Spectral_Test/
+virtualenv Spectral
+cd Spectral/
 source Scripts/activate
 ECHO Virtual Environment activated...
 ECHO Cloning Git Repo...
-git clone --branch dev https://github.com/bhans-uvic/Spectral_Detection.git
+git clone --branch main https://github.com/bhans-uvic/Spectral_Detection.git
 cd Spectral_Detection/
 ECHO Installing requirements...
 pip install -r requirements.txt
