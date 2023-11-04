81bba2c86d9152165729e4be4669e7d620dfaf2d
Konrad Jasman <kjasman@uvic.ca>
2023-11-03 23:34:45 -0700

    Added run file

---
+++ run.bat
@@ -0,0 +1,7 @@
+ECHO Virtual Environment activated...
+cd OceanEye/
+ECHO Installing requirements...
+pip install -r requirements.txt
+ECHO Done. Use the following command to start the app:
+ECHO streamlit run app.py
+streamlit run app.py
